bc6b1a106e04c58e2caab045ef275b81e283ad01
= <brunohl@letteltelecom.com.br>
2017-09-22 09:16:26 -0300

    Updates on net mask

--- app.js
+++ app.js
@@ -42,26 +42,59 @@ var Eth = function () {
     var exp = /(^192\.168\.\d{1,3}\.\d{1,3})|(^172\.16\.\d{1,3}\.\d{1,3})|(^10\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
     if (eth) { // check if isset eth
       if (eth.family && eth.family == 'IPv4') { // check only if IPV4
-        if (exp.test(eth.address)) { // match against local network
-          var n = 0;
-          var mask = eth.netmask.split('.');
-          for (var i = 0; i < mask.length; i++) {
-            if (mask[i] != '.' && mask[i] < 255) {
-              n += 8;
-            }
-          }
+        if (exp.test(eth.address)) { // match against local network, eth.netmask
           var ini = eth.address.split('.');
           ini[ ini.length - 1 ] = 0;
-          if (n > 8) {
-            ini[ ini.length - 2 ] = 0;
-          }
           eth.ini = ini.join('.');
-          eth.n = n;
+          eth.n = self.getNetworkClass( eth.netmask );
           return eth;
         }
       }
     }
     return null;
+  }
+
+  self.getNetworkClass = function ( mask ) {
+    var obj = {
+      "128.0.0.0" : "1",
+      "192.0.0.0" : "2",
+      "224.0.0.0" : "3",
+      "240.0.0.0" : "4",
+      "248.0.0.0" : "5",
+      "252.0.0.0" : "6",
+      "254.0.0.0" : "7",
+      "255.0.0.0" : "8",
+      "255.128.0.0" : "9",
+      "255.192.0.0" : "10",
+      "255.224.0.0" : "11",
+      "255.240.0.0" : "12",
+      "255.248.0.0" : "13",
+      "255.252.0.0" : "14",
+      "255.254.0.0" : "15",
+      "255.255.0.0" : "16",
+      "255.255.128.0" : "17",
+      "255.255.192.0" : "18",
+      "255.255.224.0" : "19",
+      "255.255.240.0" : "20",
+      "255.255.248.0" : "21",
+      "255.255.252.0" : "22",
+      "255.255.254.0" : "23",
+      "255.255.255.0" : "24",
+      "255.255.255.128" : "25",
+      "255.255.255.192" : "26",
+      "255.255.255.224" : "27",
+      "255.255.255.240" : "28",
+      "255.255.255.248" : "29",
+      "255.255.255.252" : "30",
+      "255.255.255.254" : "31",
+      "255.255.255.255" : "32",
+    }
+    if (obj[mask]) {
+      return obj[mask];
+    }
+    else {
+      return "/8";
+    }
   }
 
   return self;
